a6c264614e86e01c5dc7090de17550edf126ae83
Denis A <iknowcando@yandex.ru>
2022-07-06 16:40:00 +0300

    add prjct & modules

--- index.js
+++ index.js
@@ -1,3 +1,67 @@
-import _ from "lodash";
-console.log("mth fck gt!");
-console.log(_.last(['one','two']));
+
+import  readline from "readline-sync";
+import _ from "lodash"
+let name = ""
+const game = () => {
+    console.log(`Welcome to the BrAin-GaMe!`)                
+    name = readline.question('May I have your name? ');
+    console.log(`Hello, ${name}!`)
+
+}
+
+const first_game = game();
+first_game
+console.log('What is the result of the expression?') 
+let count =0;
+while (count <5){  
+    const math =  _.random(1,5);
+    let a = _.random(1,1000);
+    let b = _.random(1,999);
+    
+    let operation =""
+    let correct = 0
+    if(math===1){
+        operation+="+";
+        correct += a+b;
+    } else if (math===2){
+        operation+="-";
+        correct += a-b;
+    } else if (math===3){
+        operation+="*";
+        a =_.random(1,99)
+        b =_.random(2,9)
+        correct += a*b;
+    }else if (math===4){
+        operation+=":";
+        a =_.random(1,99)
+        b = _.random(2,9)
+        while(a%b!==0){
+            a = _.random(1,999)
+        }
+        correct += a/b;
+    } else if (math===5){
+        operation+="^";
+        a = _.random(2,9)
+        b = _.random(2,4)
+        correct += Math.pow(a,b);
+    }                                                                                                                                           
+console.log(`Question: ${a} ${operation} ${b}`)
+
+let answer = readline.question(`Your answer : `);
+let res = ""
+if (Number(answer) === correct){
+    res+="Correct!";
+    count++;
+
+} else if (Number(answer) ===0 ){
+    count =5;
+}
+else{
+    res+=`Not correct! \nCorrect answer : ${correct}`;
+    count=0;
+};
+console.log(`${res}`)
+if (count === 5){
+    console.log(`Good bye ${name}!`);
+ };
+};
